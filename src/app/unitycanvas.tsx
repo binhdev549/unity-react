d2cbef24a979b0e34b1ec9517b94b0bb3c886ee0
icii.dev <150992135+oxkhn@users.noreply.github.com>
2024-10-26 16:42:20 +0700

    5

--- src/app/unitycanvas.tsx
+++ src/app/unitycanvas.tsx
@@ -2,6 +2,26 @@
 import React, { useState, useEffect } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
+import { ArgentTMA, SessionAccountInterface } from "@argent/tma-wallet";
+import { Account } from "starknet";
+
+const argentTMA = ArgentTMA.init({
+  environment: "sepolia", // "sepolia" | "mainnet" (not supperted yet)
+  appName: "My TG Mini Test Dapp", // Your Telegram app name
+  appTelegramUrl: "https://t.me/RedBlitzBot", // Your Telegram app URL
+  sessionParams: {
+    allowedMethods: [
+      // List of contracts/methods allowed to be called by the session key
+      {
+        contract:
+          "0x036133c88c1954413150db74c26243e2af77170a4032934b275708d84ec5452f",
+        selector: "increment",
+      },
+    ],
+    validityDays: 90, // session validity (in days) - default: 90
+  },
+});
+
 function UnityCanvas() {
   const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
     loaderUrl: "build/build_rex_blitz.loader.js",
@@ -39,12 +59,72 @@ function UnityCanvas() {
     [devicePixelRatio]
   );
 
+  const [_account, setAccount] = useState<
+    SessionAccountInterface | undefined
+  >();
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Call connect() as soon as the app is loaded
+    argentTMA
+      .connect()
+      .then((res) => {
+        if (!res) {
+          // Not connected
+          setIsConnected(false);
+          return;
+        }
+
+        if (_account && _account.getSessionStatus() !== "VALID") {
+          // Session has expired or scope (allowed methods) has changed
+          // A new connection request should be triggered
+
+          // The account object is still available to get access to user's address
+          // but transactions can't be executed
+          const { account } = res;
+
+          setAccount(account);
+          setIsConnected(false);
+          return;
+        }
+
+        // Connected
+        const { account, callbackData } = res;
+        // The session account is returned and can be used to submit transactions
+        setAccount(account);
+        setIsConnected(true);
+        // Custom data passed to the requestConnection() method is available here
+        console.log("callback data:", callbackData);
+      })
+      .catch((err) => {
+        console.error("Failed to connect", err);
+      });
+  }, []);
+
+  const handleConnectButton = async () => {
+    await argentTMA.requestConnection("custom_callback_data");
+  };
+
+  // useful for debugging
+  const handleClearSessionButton = async () => {
+    await argentTMA.clearSession();
+    setAccount(undefined);
+  };
+
   return (
-    <Unity
-      unityProvider={unityProvider}
-      style={{ width: 360, height: 640 }}
-      devicePixelRatio={devicePixelRatio}
-    />
+    <div className="">
+      <button
+        onClick={handleConnectButton}
+        className="h-10 grid place-items-center rounded-md bg-slate-400 px-4"
+      >
+        <p>Connect</p>
+      </button>
+      <Unity
+        unityProvider={unityProvider}
+        style={{ width: 360, height: 640 }}
+        devicePixelRatio={devicePixelRatio}
+      />
+    </div>
   );
 }
 
